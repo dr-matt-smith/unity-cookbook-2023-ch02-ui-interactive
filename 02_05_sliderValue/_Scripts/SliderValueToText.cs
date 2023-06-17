95f0f16a10e6e792e6cb3d691f7fefa8d45b0e23
Shaun Ferns <shaun.ferns@tudublin.ie>
2023-06-17 10:35:49 +0100

    intial  commit

--- 02_05_sliderValue/_Scripts/SliderValueToText.cs
+++ 02_05_sliderValue/_Scripts/SliderValueToText.cs
@@ -1,35 +1,29 @@
-﻿﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class SliderValueToText : MonoBehaviour
 {
-	// referene to the UI slider GameObject that user will move
-	public Slider sliderUI;
+    public Slider sliderUI;
+    private TextMeshProUGUI textSliderValue;
 
-	// reference to the UI text display of slider current value
-	private Text textSliderValue;
+    void Awake()
+    {
+        textSliderValue = GetComponent<TextMeshProUGUI>();
+    }
 
-	//----------------------------
-	// get reference to Text component and store in 'textSliderValue'
-	void Awake()
-	{
-		textSliderValue = GetComponent<Text>();
-	}
+    void Start()
+    {
+        ShowSliderValue();
+    }
 
-	//----------------------------
-	// call function to update display - so as soon as scene starts we ensure correct
-	// value is displayed
-	void Start()
-	{
-		ShowSliderValue();
-	}
-
-	//-------------------------------
-	// update the text display to show current slider value as a number
-	// this method is called each time the Slider receives an OnValueChanged message
-	public void ShowSliderValue ()
-	{
-		string sliderMessage = "Slider value = " + sliderUI.value;
-		textSliderValue.text = sliderMessage;
-	}
+    public void ShowSliderValue()
+    {
+        string sliderMessage = "Slider value = " + sliderUI.value;
+        textSliderValue.text = sliderMessage;
+    }
 }
+
